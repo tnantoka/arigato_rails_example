69075ca1a7f021cf3948b7cdc06ef1409fc21c5b
tnantoka <bornneet@livedoor.com>
2013-03-29 19:59:37 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArigatoRailsExample::Application.config.secret_key_base = 'e8c14c86b3d88fe7e2aa432acc9ad092ba0c598aaa27bee1a95ed2c8c69a6def2db753d34dbc43ac3250a493d420cfbec5f7260e6ff52d7a2c953c3ad6de8f18'
